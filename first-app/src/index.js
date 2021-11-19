dce573580f31846a5cd1c70219743dddd1675023
sam <sam@mirahi.io>
2021-11-19 12:33:18 +0100

    Introduction JSX

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
 
-function HelloWorld(){
-    return <div> hi </div>; 
+function HelloWorld() {
+    const name = 'traveler';
+    /*
+    return (
+        <>
+            <div>
+                <h1> HI {name.toLocaleUpperCase()} !</h1>
+            </div>
+            <div>
+                <p> this a good exampple of sending 2 components in react </p>
+            </div>
+        </>
+    );
+    */
+
+    // What is inside the ()
+    return React.createElement('div', null,
+        React.createElement('h1', null,
+            'Hi ', name.toLocaleUpperCase(), ' !'
+        )
+
+    );
 }
 
-ReactDOM.render(<HelloWorld/>, document.querySelector('#root'));
+ReactDOM.render(<HelloWorld />, document.querySelector('#root'));
