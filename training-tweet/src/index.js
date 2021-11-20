ad603dca07b7319f0c29fcb3242445cde8d20cc9
sam <sam@mirahi.io>
2021-11-20 16:18:08 +0100

    tweet : icon + components + css

--- training-tweet/src/index.js
+++ training-tweet/src/index.js
@@ -7,7 +7,28 @@ function Tweet() {
     return (
         <div className='tweet'>
             <Avatar />
+            <div className="content">
+                <Author />
+                <Time />
+                <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton />
+                    <LikeButton />
+                    <MoreOptionsButton />
+                </div>
+            </div>
+
         </div>
+    );
+}
+
+function Author() {
+    return (
+        <span className="author">
+            <span className="name">SaNd</span>
+            <span className="handle">@jojoker33</span>
+        </span>
     );
 }
 
@@ -22,10 +43,25 @@ function Avatar() {
     );
 }
 
+function Message() {
+    return (
+        <div className="message">React is not that bad.</div>
+    );
+}
+
+function Time() {
+    return (
+        <span className="time">3h ago</span>
+    );
+}
 
 
-
-
+//Buttons
+// with anonymous function
+const ReplyButton = () => (<i className="fa fa-reply reply-button" />);
+const RetweetButton = () => (<i className="fa fa-retweet retweet-button" />);
+const LikeButton = () => (<i className="fa fa-heart like-button" />);
+const MoreOptionsButton = () => (<i className="fa fa-ellipsis-h more-options-button" />);
 
 
 ReactDOM.render(<Tweet />, document.querySelector('#root'));
