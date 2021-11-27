7fa896546f48c446900b84ea7d782eba570ef92c
sam <sam@mirahi.io>
2021-11-27 18:14:14 +0100

    Github file-list

---
+++ github-file-list/src/components/CommitMessage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function CommitMessage({ commit }) {
+    return (
+        <td className="commit-message">
+            {commit.message}
+        </td>
+
+    );
+}
+
+CommitMessage.propTypes = {
+    commit: PropTypes.shape({
+        message: PropTypes.string.isRequired
+    })
+};
--- github-file-list/src/components/FilelistItem.js
+++ github-file-list/src/components/FilelistItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FileName from './FileName';
+import CommitMessage from './CommitMessage';
+import Time from './Time';
 
 export default function FileListIem({ file }) {
 
-    return (<tr className="file-list-item">
-        <FileName file={file} />
-    </tr>);
+    return (
+        <tr className="file-list-item">
+            <FileName file={file} />
+            <CommitMessage commit={file.latestCommit} />
+            <td className="age">
+                <Time time={file.updated_at} />
+            </td>
+        </tr>);
 
 };
 
