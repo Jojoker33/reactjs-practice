f31a05444bd4b4a423ad3bc9268dd015881582f1
sam <sam@mirahi.io>
2021-12-12 15:35:17 +0100

    useEffect vs   useLayoutEffect examples

---
+++ effect_layouteffect/src/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import ReactDOM from "react-dom";
+
+const TwoStageRender = () => {
+  const [value, setValue] = useState(0);
+
+  /**
+   * useEffect will run asynchronously after the render is done and screen painted so it will cause a small flicker, quick change to 0 before update
+   */
+  //useEffect(() => {
+
+  // Warning : will block rendering so prioritize useEffect
+  useLayoutEffect(() => {
+    if (value === 0) {
+      setValue(10 + Math.random() * 200);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    if (value === 0) {
+      setValue(10 + Math.random() * 200);
+    }
+  }, [value]);
+
+  console.log("render", value);
+
+  return <div onClick={() => setValue(0)}> value: {value} </div>;
+};
+
+ReactDOM.render(<TwoStageRender />, document.querySelector("#root"));
